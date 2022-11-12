cc0a7e777d2b5835ebaca7de773c087a171079e8
Henko Meyer <henkomeyer@gmail.com>
2022-11-12 20:39:01 +0200

    Added registration Logic and data validation

--- Group2-Project-MainProgram/FileHandler.cs
+++ Group2-Project-MainProgram/FileHandler.cs
@@ -3,11 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms; 
 
 namespace Group2_Project_MainProgram
 {
     class FileHandler
     {
+        public void WritetoText(List<string> GetText)
+        {
+            try
+            {
+                File.AppendAllLines("UserDetail.txt", GetText);
+                MessageBox.Show("You are now Registered!","Welcome!",MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show( er.Message,"Something Went Wrong", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
 
+        }
     }
 }
---
+++ Group2-Project-MainProgram/UserDetail.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group2_Project_MainProgram
+{
+    class UserDetail
+    {
+        public UserDetail(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+
+        internal string Username { get; set; }
+
+        internal string Password { get; set; }
+
+        public string Storeinfo()
+        {
+            return $"{this.Username},{this.Password}"; 
+        }
+    }
+}
